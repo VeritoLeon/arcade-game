203da358ec7b1285d26a170898ff32ca8c91837c
unknown <Vero@Mellie.grant>
2015-01-13 21:55:25 -0700

    Graphics to appear on my screen. Handled the user input (got the player to move) and collisions to be detected (Thanks to Chuck Underwood at Piazza for pointing at MDN: 2D collision detection)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x0, y0, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x0;
+    this.y = y0;
+    this.width = 101;
+    this.height = 83;
+    this.speed = speed;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x * dt;
+    this.checkCollisions();
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +28,79 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Enemy.prototype.checkCollisions = function() {
+    var rect1 = {x: this.x, y: this.y, width: this.width, height: this.height}
+    var rect2 = {x: player.x, y: player.y, width: player.width, height: player.height}
+
+    if (rect1.x < rect2.x + rect2.width &&
+       rect1.x + rect1.width > rect2.x &&
+       rect1.y < rect2.y + rect2.height &&
+       rect1.height + rect1.y > rect2.y) {
+        console.log('Game over');
+    }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x0, y0, speed) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x0;
+    this.y = y0;
+    this.width = 101;
+    this.height = 83;
+    this.speed = speed;
+}
 
+Player.prototype.update = function(dt){}
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(direction){
+    var maxRowIndex = 5,
+        maxColIndex = 4,
+        currentRow = Math.floor(this.y/83),
+        currentCol = Math.floor(this.x/101);
+        console.log('currentRow: ' + currentRow);
+        console.log('currentCol: ' + currentCol);
+
+    switch(direction) {
+        case 'left':
+            if (currentCol > 0) {
+                this.x = (currentCol - 1) * 101;
+            }
+            break;
+        case 'up':
+            if (currentRow > 0) {
+                this.y = (currentRow - 1) * 83;
+            }
+            break;
+        case 'right':
+            if(currentCol < maxColIndex) {
+                this.x = (currentCol + 1) * 101;
+            }
+            break;
+        case 'down':
+            if(currentRow < maxRowIndex) {
+                this.y = (currentRow + 1) * 83;
+            }
+            break;
+    }
+    currentRow = Math.floor(this.x/83),
+    currentCol = Math.floor(this.y/101),
+
+    console.log('currentRow: ' + currentRow);
+    console.log('currentCol: ' + currentCol);
+}
 
 // Now instantiate your objects.
+var player = new Player(202, 83*5, 1);
+var enemy1 = new Enemy(0,0,1);
+var enemy2 = new Enemy(101,83,1);
+var enemy3 = new Enemy(202,166,1);
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [enemy1, enemy2, enemy3];
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +115,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
